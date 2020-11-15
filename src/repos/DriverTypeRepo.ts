b1d956efdc654d5fdf26413b6db173f86fd6ad79
josecoutinho1180622 <1180622@isep.ipp.pt>
2020-11-15 21:23:11 +0000

    #5 Create Driver Type - Repository added with save

---
+++ src/repos/DriverTypeRepo.ts
@@ -0,0 +1,42 @@
+import { Document, Model } from "mongoose";
+import { Inject, Service } from "typedi";
+import { IDriverTypePersistence } from "../dataschema/IDriverTypePersistence";
+import { DriverType } from "../domain/driverType";
+import { DriverTypeMap } from "../mappers/DriverTypeMap";
+import IDriverTypeRepo from "./IRepos/IDriverTypeRepo";
+
+@Service()
+export default class DriverTypeRepo implements IDriverTypeRepo {
+    constructor(
+        @Inject('driverTypeSchema') private driverTypeSchema: Model<IDriverTypePersistence & Document>,
+    ) { }
+
+    exists(t: DriverType): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async save(driverType: DriverType): Promise<DriverType> {
+
+        const query = { desc: driverType.description }
+
+        const driverTypeDocument = await this.driverTypeSchema.findOne(query)
+
+        try {
+            if (driverTypeDocument === null) {
+                const rawDriverType: any = DriverTypeMap.toPersistence(driverType)
+
+                const driverTypeCreated = await this.driverTypeSchema.create(rawDriverType)
+
+                return driverType
+            } else {
+                driverTypeDocument.description = driverType.description
+                await driverTypeDocument.save()
+
+                return driverType
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+}
---
+++ src/repos/IRepos/IDriverTypeRepo.ts
@@ -0,0 +1,5 @@
+import { Repo } from "../../core/infra/Repo";
+import { DriverType } from '../../domain/driverType';
+
+export default interface IDriverTypeRepo extends Repo<DriverType> {
+}
