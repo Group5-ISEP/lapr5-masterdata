591cebd51267caa67d3a95e1cb8ecf49830cb40d
josecoutinho1180622 <1180622@isep.ipp.pt>
2020-11-15 18:11:40 +0000

    #5 Create Driver Type - Service - interface for driver type service created

---
+++ src/services/IServices/IDriverTypeService.ts
@@ -0,0 +1,6 @@
+import { Result } from "../../core/logic/Result";
+import IDriverTypeDTO from "../../dto/IDriverTypeDTO";
+
+export default interface IDriverTypeService  {
+  createDriverType(driverTypeDTO: IDriverTypeDTO): Promise<Result<IDriverTypeDTO>>;
+}
