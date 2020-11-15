a55f466906b4bdf149a3c8138630ffba569ff0ec
josecoutinho1180622 <1180622@isep.ipp.pt>
2020-11-15 20:47:00 +0000

    #5 Create Driver Type - schema created for persistence

--- src/persistence/schemas/driverTypeSchema.ts
+++ src/persistence/schemas/driverTypeSchema.ts
@@ -1,11 +1,15 @@
-import { IUserPersistence } from '../../dataschema/IUserPersistence';
 import mongoose from 'mongoose';
+import { IDriverTypePersistence } from '../../dataschema/IDriverTypePersistence';
 
 const DriverType = new mongoose.Schema(
     {
-        description: { type: String }
+        description: {
+            type: String,
+            required: [true, 'Please enter type description'],
+            index: true
+        },
     },
     { timestamps: true },
 );
 
-export default mongoose.model<IUserPersistence & mongoose.Document>('DriverTypeSchema', DriverType);
+export default mongoose.model<IDriverTypePersistence & mongoose.Document>('DriverType', DriverType);
