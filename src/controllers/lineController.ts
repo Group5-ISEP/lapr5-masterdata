291413b43b958b1f807be8cd9ef0d7102c915603
josecoutinho1180622 <1180622@isep.ipp.pt>
2020-11-16 23:32:52 +0000

    #3 Create Line - controller implemented

--- src/controllers/lineController.ts
+++ src/controllers/lineController.ts
@@ -1,30 +1,29 @@
 import { Request, Response, NextFunction } from "express";
-import { config } from "process";
 import { Inject } from "typedi";
 import { Result } from "../core/logic/Result";
 import ILineController from "./IControllers/ILineController";
-
+import config from '../../config';
+import ILineService from "../services/IServices/ILineService";
+import ILineDTO from "../dto/ILineDTO";
 export default class LineController implements ILineController /* TODO: extends ../core/infra/BaseController */ {
-    /*   constructor(
-           @Inject(config.services.line.name) private lineServiceInstance: ILineService
-       ) { }
-   */
+    constructor(
+        @Inject(config.services.line.name) private lineServiceInstance: ILineService
+    ) { }
+
     public async createLine(req: Request, res: Response, next: NextFunction) {
-        /* try {
-             const LineOrError = await this.LineServiceInstance.createLine(req.body as ILineDTO) as Result<ILineDTO>;
- 
-             if (LineOrError.isFailure) {
-                 return res.status(402).send();
-             }
- 
-             const LineDTO = LineOrError.getValue();
-             return res.status(201).json(LineDTO);
-         }
-         catch (e) {
-             return next(e);
-         }
- */
-        res.status(200).send(req.body)
+        try {
+            const lineOrError = await this.lineServiceInstance.createLine(req.body as ILineDTO) as Result<ILineDTO>;
+
+            if (lineOrError.isFailure) {
+                return res.status(402).send();
+            }
+
+            const LineDTO = lineOrError.getValue();
+            return res.status(201).json(LineDTO);
+        }
+        catch (e) {
+            return next(e);
+        }
     };
 
 }
