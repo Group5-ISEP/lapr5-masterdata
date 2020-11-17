d7b9f2538910f7aa5fbb23767fcccc88153ace29
josecoutinho1180622 <1180622@isep.ipp.pt>
2020-11-17 15:25:13 +0000

    #3 Create Line - line mapper added

---
+++ src/mappers/LineMap.ts
@@ -0,0 +1,49 @@
+import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+import { Mapper } from "../core/infra/Mapper";
+import ILinePersistence from "../dataschema/ILinePersistence";
+import { Line } from "../domain/line";
+import ILineDTO from "../dto/ILineDTO";
+
+export class LineMap extends Mapper<Line>{
+
+    public static toDTO(line: Line): ILineDTO {
+        return {
+            code: line.code,
+            name: line.name,
+            terminalNodes: line.terminalNodes,
+            colorRGB: line.colorRGB,
+            allowedDriverTypes: line.allowedDriverTypes,
+            allowedVehicleTypes: line.allowedVehicleTypes
+        } as ILineDTO;
+    }
+
+    public static toDomain(lineRaw: any): Line {
+
+        const lineOrError = Line.create(
+            {
+                code: lineRaw.code,
+                name: lineRaw.name,
+                terminalNodes: lineRaw.terminalNodes,
+                colorRGB: lineRaw.colorRGB,
+                allowedDriverTypes: lineRaw.allowedDriverTypes,
+                allowedVehicleTypes: lineRaw.allowedVehicleTypes
+            },
+            new UniqueEntityID(lineRaw._id)
+        );
+
+        lineOrError.isFailure ? console.log(lineOrError.error) : '';
+
+        return lineOrError.isSuccess ? lineOrError.getValue() : null;
+    }
+
+    public static toPersistence(line: Line): ILinePersistence {
+        return {
+            code: line.code,
+            name: line.name,
+            terminalNodes: line.terminalNodes,
+            colorRGB: line.colorRGB,
+            allowedDriverTypes: line.allowedDriverTypes,
+            allowedVehicleTypes: line.allowedVehicleTypes
+        } as ILinePersistence
+    }
+}
