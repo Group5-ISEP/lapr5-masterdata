a785273dbfa3715849998034695bb2f77d56b4fd
patseho <juanv4004@gmail.com>
2020-11-22 19:32:14 +0000

    fixed structure of project

--- config.js
+++ config.js
@@ -19,7 +19,8 @@ export default {
   /**
    * That long string from mlab
    */
-  databaseURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/test',
+    databaseURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/test',
+    //mongodb+srv://admin:admin@clustergrupo5.6nnre.mongodb.net/masterdata?authSource=admin&replicaSet=atlas-113yi8-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true
 
   /**
    * Your secret sauce
