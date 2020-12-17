f8edd69972d80bb0102468c6419b929e19b485fe
zemiguel20 <migli.coutinho@gmail.com>
2020-12-17 17:14:06 +0000

    #6 Create vehicle type - domain id added to schema and some more details to other atributes

--- src/persistence/schemas/vehicleTypeSchema.ts
+++ src/persistence/schemas/vehicleTypeSchema.ts
@@ -3,17 +3,41 @@ import IVehicleTypePersistence from '../../dataschema/IVehicleTypePersistence';
 
 const vehicleType = new mongoose.Schema(
     {
+        id: {
+            type: String,
+            required: [true, 'Please enter type id'],
+            index: true,
+            unique: true
+        },
         name: {
             type: String,
             required: [true, 'Please enter type name'],
-            index: true
+            unique: true
         },
-        autonomy: Number,
-        costByKm: Number,
-        averageConsumption: Number,
-        averageSpeed: Number,
-        emissions: Number,
-        energySource: String
+        autonomy: {
+            type: Number,
+            required: [true, 'Please enter type autonomy'],
+        },
+        costByKm: {
+            type: Number,
+            required: [true, 'Please enter type cost by km'],
+        },
+        averageConsumption: {
+            type: Number,
+            required: [true, 'Please enter type average consumption'],
+        },
+        averageSpeed: {
+            type: Number,
+            required: [true, 'Please enter type average speed'],
+        },
+        emissions: {
+            type: Number,
+            required: [true, 'Please enter type emissions'],
+        },
+        energySource: {
+            type: String,
+            required: [true, 'Please enter type energy source']
+        }
     },
     { timestamps: true },
 );
